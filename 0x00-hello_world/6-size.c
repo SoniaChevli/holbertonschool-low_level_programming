681b54196a6d9c8c3e33647fc3aea255cf0eb9a5
SoniaChevli <you@example.com>
2018-01-18 01:19:49 +0000

    Make Betty happy

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
- int bytesizeint;
- char bytesizechar;
- long int bytesizelong;
- long long int bytesizelonglong;
- float bytesizefloat;
+char bytesizechar;
+int bytesizeint;
+long int bytesizelong;
+long long int bytesizelonglong;
+float bytesizefloat;
 
- printf("Size of a char: %d byte(s)\n", sizeof(bytesizechar));
- printf("Size of an int: %d byte(s)\n", sizeof(bytesizeint));
- printf("Size of a long int: %d byte(s)\n", sizeof(bytesizelong));
- printf("Size of a long long int: %d byte(s)\n", sizeof(bytesizelonglong));
- printf("Size of a float: %d byte(s)\n", sizeof(bytesizefloat));
+printf("Size of a char: %d byte(s)\n", sizeof(bytesizechar));
+printf("Size of an int: %d byte(s)\n", sizeof(bytesizeint));
+printf("Size of a long int: %d byte(s)\n", sizeof(bytesizelong));
+printf("Size of a long long int: %d byte(s)\n", sizeof(bytesizelonglong));
+printf("Size of a float: %d byte(s)\n", sizeof(bytesizefloat));
 return (0);
 }

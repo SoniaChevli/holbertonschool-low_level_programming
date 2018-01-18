17d29b6c6e44fa103247f955e98614b203379534
SoniaChevli <you@example.com>
2018-01-18 23:03:42 +0000

    Create 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - print all digiits 0-9
+ * Return: all digits. Otherwise 0.
+ */
+int main(void)
+{
+	int i = 0;
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}

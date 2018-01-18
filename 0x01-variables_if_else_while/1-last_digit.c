f34f0e4d6e009d9151bc8a5aa004a8a600554218
SoniaChevli <you@example.com>
2018-01-18 20:23:04 +0000

    Add header

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - find the last digit of random number
+ * Return: print what that last digit is and how it relates to 5
+ */
+
 int main(void)
 {
   int n;
@@ -12,11 +16,11 @@ int main(void)
   n = rand() - RAND_MAX / 2;
   
   x = n % 10;
-  if (n > 5)
+  if (x > 5)
     printf("Last digit of %d is %d and is greater than 5\n", n, x);
-  if (n = 0)
+  if (x = 0)
     printf("Last digit of %d is %d and is zero\n", n, x);
-  if (n < 6)
+  if (x < 6)
     printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
   return (0);
 }

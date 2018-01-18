ffed1d2de33801b771c086c5ad5d568ca4595148
SoniaChevli <you@example.com>
2018-01-18 01:01:07 +0000

    Type changed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,15 @@
 int main(void)
 {
  int bytesizechar;
- int bytesizeint;
- int bytesizelong;
- int bytesizelonglong;
- int bytesizefloat;
+ char bytesizeint;
+ long bytesizelong;
+ longlong bytesizelonglong;
+ float bytesizefloat;
 
-  printf("Size of a char:%d byte(s)\n", bytesizechar);
-  printf("Size of an int:%d byte(s)\n", bytesizeint);
-  printf("Size of a long int:%d byte(s)\n", bytesizelong);
-  printf("Size of a long long int:%d byte(s)\n", bytesizelonglong);
-  printf("Size of a float:%d byte(s)\n", bytesizefloat);
+  printf("Size of a char: %d byte(s)\n", bytesizechar);
+  printf("Size of an int: %d byte(s)\n", bytesizeint);
+  printf("Size of a long int: %d byte(s)\n", bytesizelong);
+  printf("Size of a long long int: %d byte(s)\n", bytesizelonglong);
+  printf("Size of a float: %d byte(s)\n", bytesizefloat);
 return (0);
 }

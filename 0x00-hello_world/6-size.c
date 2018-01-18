35c08cbc17fd358f93b414e0e30a5d8178c2b255
SoniaChevli <you@example.com>
2018-01-18 00:27:36 +0000

    create 6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char:%s byte(s)", 2)
+printf("Size of an int: %s byte(s)", 4)
+printf("Size of a long int:%s byte(s)", 4)
+printf("Size of a long long int: %s byte(s)", 8)
+printf("Size of a float: %s byte(s)", 4)
+return (0);
+}

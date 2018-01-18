f0ea450de03330f91fdcff37011de41a9358bf31
SoniaChevli <you@example.com>
2018-01-18 00:55:42 +0000

    Add { include variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,17 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-  int = bytesizechar;
-int = bytesizeint;
-int = bytesizelong;
-  int = bytesizelonglong;
-  int = bytesizefloat;
 {
+ int bytesizechar;
+ int bytesizeint;
+ int bytesizelong;
+ int bytesizelonglong;
+ int bytesizefloat;
+
   printf("Size of a char:%d byte(s)\n", bytesizechar);
-  printf("Size of an int: %d byte(s)\n", bytesizeint);
+  printf("Size of an int:%d byte(s)\n", bytesizeint);
   printf("Size of a long int:%d byte(s)\n", bytesizelong);
-  printf("Size of a long long int: %d byte(s)\n", bytesizelonglong);
-  printf("Size of a float: %d byte(s)\n", bytesizefloat);
+  printf("Size of a long long int:%d byte(s)\n", bytesizelonglong);
+  printf("Size of a float:%d byte(s)\n", bytesizefloat);
 return (0);
 }

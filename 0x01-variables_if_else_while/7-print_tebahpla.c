6f4abd759c2be58bf934fddc91d8c0021f0b9928
SoniaChevli <you@example.com>
2018-01-19 00:19:13 +0000

    Create 7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in reverse
+ * Return: alphabet in reverse. Otherwise 0
+ */
+int main(void)
+{
+	char n = 'z';
+
+	while (n >= 'a')
+	{
+		putchar(n);
+		n--;
+	}
+		putchar ('\n');
+		return (0);
+
+}

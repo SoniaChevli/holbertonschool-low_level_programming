3a3b3c433ec40782b1351295c329490f6aa4166d
SoniaChevli <you@example.com>
2018-01-19 00:35:05 +0000

    Create 8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - print all hexidecimal numbers in lowercaser
+ * Return: All hexidecimal numbers in lower case. Otherwise 0
+ */
+int main(void)
+{
+	int i = 0;
+	char n = 'a';
+
+	while (i <= 9)
+	{
+		putchar('0' + i);
+		i++;
+	}
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}

804ea761113babe7b088184e2e3eac8a84f77b81
SoniaChevli <you@example.com>
2018-01-23 20:46:06 +0000

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+/Users/sonia #include "holberton.h"
+/**
+ * print_sign-print the sign of the number
+ * Return: 1 or 0 or -1
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}

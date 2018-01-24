e4afde6f01531fddc7a5da1d94dd575e0e1129ae
SoniaChevli <you@example.com>
2018-01-24 00:05:53 +0000

    Create 9-timestable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * times_table - print times table
+ */
+void times_table(void)
+{
+	int x = 0;
+	int i = 0;
+	int y;
+
+	while (x <= 9)
+	{
+		while (i <= 9)
+		{
+			y = i * x;
+			if (y <= 9)
+			{
+				_putchar(' ');
+				_putchar ('0' + y);
+			}
+			else
+			{
+				_putchar ('0' + y / 10);
+				_putchar ('0' + y % 10);
+			}
+			if (i != 9)
+			{
+				_putchar(',');
+			}
+			_putchar(' ');
+			i++;
+		}
+		_putchar('\n');
+		i = 0;
+		x++;
+	}
+}

e7217089e16199d1169ec41cc882c10aa66401b4
SoniaChevli <you@example.com>
2018-01-24 00:29:49 +0000

    Create 10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - add the two integers
+ * Return: number. Otherwise 0
+ *@x: number to be added
+ *@y: other number
+ */
+
+int add(int x, int y)
+{
+	int z = x + y;
+
+	return (z);
+}

d1f5d98ec63e6a318a6b68dfa3d25c4e233016b6
SoniaChevli <you@example.com>
2018-01-24 01:43:35 +0000

    Make Betty Happy

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,12 @@
 /**
  * _isalpha - check for alphabetic character
  * Return: 1 if c is a letter. Otherwise 0
+ *@c: random letter
  */
 
 int _isalpha(int c)
 {
-	if( c <= 'z' && c >= 'A')
+	if (c <= 'z' && c >= 'A')
 		return (1);
 	return (0);
 }

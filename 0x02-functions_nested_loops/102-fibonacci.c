8df48eb58b4101b20af0d3286d77479c7e714679
SoniaChevli <you@example.com>
2018-01-25 00:37:29 +0000

    Change first digit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-
+/**
+ *main - first 50 digits of fibonacci
+ * Return: first 50 digits. Otherwise 0
+ */
 int main(void)
 {
-	long int x = 0;
-	long int y = 1;
+	long int x = 1;
+	long int y = 2;
 	long int z;
 	long int count = 0;
 
-	printf("0, 1, ,");
+	printf("1, 2, ");
 	while (count <= 48)
 	{
 		z = x + y;
 		printf("%li", z);
 		if (count != 48)
-			printf(", ,");
+			printf(", ");
 		x = y;
 		y = z;
 		count++;

749e32c4efffb641990b3056c058c29c8bd8ed36
SoniaChevli <you@example.com>
2018-01-26 00:55:22 +0000

    Create 2-mul.c

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *mul - multiplies 2 numbers
+ *Return: multiplication answer. Otherwise 0
+ *
+ *@a: first number
+ *@b: second number
+ */
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}

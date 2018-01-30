81ebb0e7d96b4c30c0ba18baa02b03f7d0debc1a
SoniaChevli <you@example.com>
2018-01-30 22:23:28 +0000

    Add print rev

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+
+#include "holberton.h"
+/**
+ * print_rev - print a string in reverse
+ *
+ *@s: string
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}

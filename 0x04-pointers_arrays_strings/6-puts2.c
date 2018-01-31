95edde7596cbbe3c49ab137f30e897dd2507dc53
SoniaChevli <you@example.com>
2018-01-31 21:48:15 +0000

    Dont over add

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,15 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (*(str + i) != '\0')
 	{
-		_putchar(str[i]);
+		_putchar(*(str +i));
+		if (i + 1 != '\0')
 		i += 2;
+		else
+		{
+			i++;
+		}
 	}
 	_putchar('\n');
 }

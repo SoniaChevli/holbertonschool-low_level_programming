3d67ca1181baaf8d18fe8f0ca0a7f7f7eae1a22d
SoniaChevli <you@example.com>
2018-01-31 03:10:34 +0000

    Create 7-puts_half.c

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * puts_half - pritn half of string
+ *
+ *@str: string
+ */
+void puts_half(char *str)
+{
+	int i;
+	int y;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		i++;
+	}
+	if (i % 2 != 0)
+		y = ((i - 1) / 2);
+	else
+		y = i / 2;
+	while (y <= i)
+	{
+		_putchar(*(str + y));
+		y++;
+	}
+
+	_putchar('\n');
+}

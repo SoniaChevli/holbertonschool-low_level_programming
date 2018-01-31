c13fa36c3ffe1327de466631ef6ec0c995edb574
SoniaChevli <you@example.com>
2018-01-31 23:22:14 +0000

    Create 7-puts_half

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -9,16 +9,15 @@ void puts_half(char *str)
 	int i;
 	int y;
 
-	for (i = 1; *(str + i) != '\0'; i++)
+	for (i = 0; *(str + i) != '\0'; i++)
 	{
 		i++;
 	}
-	i--;
 	if (i % 2 != 0)
 		y = ((i - 1) / 2);
 	else
 		y = i / 2;
-
+	
 	while (y <= i)
 	{
 		_putchar(*(str + y));

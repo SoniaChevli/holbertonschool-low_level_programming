5288618c15440f4c02cad303203a07178fefa8ee
SoniaChevli <you@example.com>
2018-02-02 03:53:37 +0000

    Create 8-rot13

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -13,16 +13,19 @@ char *rot13(char *s)
 	int x = 0;
 
 	while (s[i] != '\0')
-        {
+	{
 		while (arr[x] != '\0')
 		{
-		if (arr[x] == s[i])
-		    s[i] = rot[x];
-		    x++;
+			if (arr[x] == s[i])
+			{
+				s[i] = rot[x];
+				break;
+			}
+			x++;
 		}
 		i++;
 		x = 0;
-        }
-        return (s);
+	}
+	return (s);
 
 }

b4fcff38e4d947eedd749c4a3ea2ae4af146f792
SoniaChevli <you@example.com>
2018-02-02 05:49:09 +0000

    Add init

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -9,9 +9,12 @@ char *cap_string(char *s)
 {
 	int i = 0;
 
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] -= 32;
+
 	while (s[i] != '\0')
 	{
-		switch(s[i])
+		switch (s[i])
 		{
 		case ' ':
 		case '\t':

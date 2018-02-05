f0af821e6a0619c7638ceb6ad9150a36f6b8aa17
SoniaChevli <you@example.com>
2018-02-05 22:38:56 +0000

    Create 5-strstr.c

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * _strstr - locates a substring
+ * Return: a pointer to te beginning of the located substring of NULL
+ *
+ *@haystack: c string to be scanned
+ *@needle: c string containing the sequence of characters to match
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int x = 0;
+	int i = 0;
+	int length = 0;
+	int count;
+
+	while (needle[length] != '\0')
+		length++;
+
+	while (haystack[i] != '\0')
+	{
+		count = i;
+		while (x <= length)
+		{
+			if (needle[x] != haystack[count])
+			{
+				break;
+			}
+			x++;
+			count++;
+		}
+
+		if (length == x - 1)
+			return (needle);
+
+		x = 0;
+		i++;
+	}
+	return (0);
+}

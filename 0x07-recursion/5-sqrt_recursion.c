ace0a6fbf6fc216a8327fc2147fa0cdf309b28e1
SoniaChevli <you@example.com>
2018-02-07 23:59:51 +0000

    create 5-sqrt_recurssion.c

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,23 +1,32 @@
 #include "holberton.h"
 /**
- * _sqrt_recursion - finds narural square root
+ * innerfunc - Does the recurssion
  *Return: natual square root. Otherwise -1
  *
  *@n: number to find square root
+ *@x: is n but decremented
+*/
+
+int innerfunc(int x, int n)
+{
+	int y;
+
+	if (x <= 0)
+		return (-1);
+	y = n / x;
+
+	if (y == x && n % x == 0)
+		return (x);
+	else
+		return (innerfunc(x - 1, n));
+}
+/**
+ * _sqrt_recursion - finds narural square root
+ * Return: n twice
+ *@n: number
  */
+
 int _sqrt_recursion(int n)
 {
-	return (innerfunc(n, n));
+return (innerfunc(n, n));
 }
-int innerfunc(int x, int n)
-	{
-		int y;
-
-		y = n / x;
-		if (x == 0)
-			return (-1);
-		if (y == x)
-			return (x);
-		else
-			return (innerfunc(x-1,n));
-	}

4bbd3f97d0440a1f05c85b4457916524b4efe918
SoniaChevli <you@example.com>
2018-02-14 22:44:11 +0000

    Compile correctly

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -11,21 +11,25 @@ int **alloc_grid(int width, int height)
 {
 	int **arr;
 	int i;
+	int x;
 
 	arr = malloc(sizeof(int *) * height);
-	if (arr <= 0)
+	if (arr == 0)
+		return (0);
+	if (width <= 0 || height <= 0)
 		return (0);
 
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(width * sizeof(int));
 
-		if (arr[i] <= 0)
+		if (arr[i] == 0)
 		{
-			while (i > 0)
+			x = i;
+			while (x > 0)
 			{
-				free(arr[i]);
-				i--;
+				free(arr[x]);
+				x--;
 			}
 			free(arr);
 			return (0);

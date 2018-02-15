f8a69fca7d99686bcd3974175b28e5926e0802eb
SoniaChevli <you@example.com>
2018-02-15 20:34:49 +0000

    Create 2-calloc.c

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *_calloc - allocates memory for an array using malloc
+ *Return: a pointer to the alocated memory. Otherwise 0.
+ *
+ *@nmemb: amount of elements
+ *@size: size of bytes
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (0);
+
+	arr = malloc(nmemb * sizeof(size));
+	if (arr == 0)
+		return (0);
+	return (arr);
+}

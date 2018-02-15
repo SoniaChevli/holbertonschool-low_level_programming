78339b2f044f56c9140bdc100364002b7db04029
SoniaChevli <you@example.com>
2018-02-15 19:39:10 +0000

    Create 0-malloc_checked

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * malloc_checked - allocates memory using malloc
+ * Return: allocated memory
+ *
+ *@b: space that needs to be allocated
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *arr;
+
+	arr = malloc(b * sizeof(int));
+	if (arr == 0)
+		exit(98);
+	return (arr);
+}

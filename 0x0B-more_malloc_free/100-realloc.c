ab12931f036c0595eab83c0b0c9740bcfcc5a605
SoniaChevli <you@example.com>
2018-02-16 22:25:46 +0000

    Account for old being greater

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -13,22 +13,23 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *arr;
 	unsigned int i;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr == 0)
-		return (0);
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (0);
 	}
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == 0)
+		return (0);
+
 
 
 	arr = malloc(new_size);
 	if (arr == 0)
 		return (0);
 
-	for (i = 0; i < new_size; i++)
+	for (i = 0; i < new_size && i < old_size; i++)
 	{
 		arr[i] = ((char *)ptr)[i];
 	}

1bafe03e465be9029d634133b527a63f22c3d65c
SoniaChevli <you@example.com>
2018-02-19 19:10:57 +0000

    Create dog.h

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - characteristics of the dog
+ *
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: who owns the dpg
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif

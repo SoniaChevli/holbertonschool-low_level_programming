17dd9e87fee3bda3c6a88c0afade2667a3c8ea75
SoniaChevli <you@example.com>
2018-02-19 23:59:04 +0000

    Remove copy

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -12,44 +12,15 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *newdog;
-	char *arr;
-	int i, j, x, y;
-	x = 0;
-	y = 0;
 
 	newdog = malloc(sizeof(struct dog));
 
 	if (newdog == 0)
 		return (0);
 
-	while (name[i] != '\0')
-	{
-		i++;
-	}
-
-	while (owner[j] != '\0')
-	{
-		j++;
-	}
-	arr = malloc(sizeof(char) * (i + j + 2));
-	if (arr == 0)
-		return (0);
 	newdog->name = name;
 	newdog->age = age;
 	newdog->owner = owner;
-	while (x < i)
-	{
-		arr[x] = name[x];
-		x++;
-	}
-	printf(" ");
-	while (y < j)
-	{
-		arr[x] = owner[y];
-		x++;
-		y++;
-	}
-	arr[x] = '\0';
 
-	return newdog;
+	return (newdog);
 }

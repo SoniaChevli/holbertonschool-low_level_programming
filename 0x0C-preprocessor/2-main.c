f6f85dadc1d6ec7fd11c996fc565078718712c1d
SoniaChevli <you@example.com>
2018-02-19 17:53:24 +0000

    2-main.c

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - returns name of file
+ * Return: file name. Otherwise 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

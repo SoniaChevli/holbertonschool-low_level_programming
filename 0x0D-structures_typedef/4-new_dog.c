5c6b6383348cf334d393274645d7ca316783aad2
SoniaChevli <you@example.com>
2018-02-20 00:43:10 +0000

    Change

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+int _strlen(char *s)
+{
+	int length = 0;
+	while (s[length] != '\0')
+		length++;
+	return (length);
+}
 /**
  *new_dog - creates a new dog
  *Return: new dog. Otherwise NULL.
@@ -12,6 +19,11 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *newdog;
+	int namelen = _strlen(name);
+	int ownlen = _strlen(owner);
+	char *arrnam;
+	char *arrown;
+	int i;
 
 	newdog = malloc(sizeof(struct dog));
 
@@ -21,6 +33,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	newdog->name = name;
 	newdog->age = age;
 	newdog->owner = owner;
+	arrnam = malloc(sizeof(char) * namelen + 1);
+
+	if (newdog->name == 0)
+		return (0);
+
+	for (i = 0; name[i] <= '\0'; i++)
+	{
+		arrnam[i] = name[i];
+	}
+
+	arrown = malloc(sizeof(char) * ownlen + 1);
+
+	if (newdog->owner == 0)
+		return (0);
+	for (i = 0; owner[i] != '\0'; i++)
+		arrown[i] = owner[i];
 
 	return (newdog);
 }

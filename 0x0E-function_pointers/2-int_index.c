2519d181ac5df05f64b5d1956f71cf0ae1ef2389
SoniaChevli <you@example.com>
2018-02-21 23:49:19 +0000

    Fix edge case

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -14,17 +14,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 	int result;
 
-	if (array != 0 && cmp != 0)
-	{
-
-		for (i = 0; i < size; i++)
-		{
-			result = cmp(array[i]);
-			if (result != 0)
-				return (i);
-		}
+	if (array == 0 || cmp == 0)
 		return (-1);
 
+	for (i = 0; i < size; i++)
+	{
+		result = cmp(array[i]);
+		if (result != 0)
+			return (i);
 	}
-	return (0);
+	return (-1);
 }

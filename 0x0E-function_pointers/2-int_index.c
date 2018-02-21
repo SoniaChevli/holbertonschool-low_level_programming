89b0d5667db89156b132782ffc9f16e3ccab8092
SoniaChevli <you@example.com>
2018-02-21 21:49:41 +0000

    Add edge cases

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -14,14 +14,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 	int result;
 
-	if (array == 0 || cmp == 0)
-		return (0);
-
-	for (i = 0; i < size; i++)
+	if (array != 0 && cmp != 0)
 	{
-		result = cmp(array[i]);
-		if (result != 0)
-			return (i);
+
+		for (i = 0; i < size; i++)
+		{
+			result = cmp(array[i]);
+			if (result != 0)
+				return (i);
+		}
+		return (-1);
+
 	}
-	return (-1);
+	return (0);
 }

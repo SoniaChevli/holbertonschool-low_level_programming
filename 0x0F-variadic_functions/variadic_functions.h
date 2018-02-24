428ad8c9ce66bc35b50b813a84d386cc8ded8c33
SoniaChevli <you@example.com>
2018-02-24 20:14:16 +0000

    Betty

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -3,10 +3,5 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-typedef struct arg{
-	char *c;
-	int i;
-	float f;
-	char *s;
-}arg;
+void print_all(const char * const format, ...);
 #endif

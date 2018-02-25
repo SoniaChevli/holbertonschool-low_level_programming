d80eb91d3b3cb58b366cb13e36b07c033a77e2ff
SoniaChevli <you@example.com>
2018-02-25 15:35:47 +0000

    Change print c to print s

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -14,23 +14,24 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *conversion;
 
-	if (n == 0)
-		return;
-	va_start(x, n);
-	for (i = 0; i < n; i++)
+	if (n != 0)
 	{
-		conversion = va_arg(x, char *);
-
-		if (conversion == NULL)
+		va_start(x, n);
+		for (i = 0; i < n; i++)
 		{
-			printf("nil");
-			return;
+			conversion = va_arg(x, char *);
+
+			if (conversion == NULL)
+			{
+				printf("nil");
+				return;
+			}
+
+			printf("%s", conversion);
+
+			if (i != n - 1 && separator != NULL)
+				printf("%s", separator);
 		}
-
-		printf("%s", conversion);
-
-		if (i != n - 1 && separator != NULL)
-			printf("%c", *separator);
 	}
 	printf("\n");
 	va_end(x);

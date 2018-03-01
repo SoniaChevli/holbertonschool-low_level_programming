aa47875e2eadafdd527cb38a22fc48edfbfd2378
SoniaChevli <you@example.com>
2018-03-01 18:38:19 +0000

    Create 2-add_node.c

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+/**
+ * _strnlength - finds length of string
+ * @str: string to find the length of
+ *
+ *Return: Length of string
+ */
+int _strnlength(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ * add_node - function adds a new node at the beginning of the list_t
+ *@head: head of the list
+ *@str: string that needs to be duplicated
+ *
+ *Return: Address of the new element. Othwerise NULL.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *add;
+
+	add = malloc(sizeof(list_t));
+	if (add == NULL || str == NULL)
+		return (0);
+
+	add->str = strdup(str);
+	add->len = _strnlength(add->str);
+	add->next = *head;
+	*head = add;
+	return (add);
+}

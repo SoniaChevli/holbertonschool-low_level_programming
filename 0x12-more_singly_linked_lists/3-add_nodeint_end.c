8434a21ea86aea7978f46b64d86f7177002f04b1
SoniaChevli <you@example.com>
2018-03-05 22:27:13 +0000

    Remove if n = null statement

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,11 +13,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new, *tmp;
 
 	new = malloc(sizeof(listint_t));
-	if (new == NULL || n == '\0')
-	{
-		free(new);
+	if (new == NULL)
 		return (NULL);
-	}
 
 	new->n = n;
 	new->next = NULL;

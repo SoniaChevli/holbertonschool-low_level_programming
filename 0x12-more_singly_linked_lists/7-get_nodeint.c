f826873a1eb398428f7226a17f9571894d3fd646
SoniaChevli <you@example.com>
2018-03-05 22:44:46 +0000

    Create 7-get_nodeint.c

---
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *get_nodeint_at_index - finds the nth node of the listint_t list
+ *@head: first in the list
+ *@index: index of node to find
+ *
+ *Return: nth node. Otherwise null
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp;
+	unsigned int count = 0;
+
+	tmp = head;
+	while (count != index && tmp != NULL)
+	{
+		tmp = tmp->next;
+		count++;
+	}
+	return (tmp);
+
+}

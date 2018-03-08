47c73ac3d337d8ba3a97ea10b9b9ed3dddf2a16d
SoniaChevli <soniachevli@gmail.com>
2018-03-08 09:25:00 +0000

    Account for if n is null byte

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -1,14 +1,18 @@
 #include "holberton.h"
 /**
- *get_bit - finds the bit at a given index
+ * get_bit - finds the bit at a given index
  *@n: number
  *@index: index to find value
+ *
+ *Return: value at index. Otherwise -1
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int compare = 1;
 
 	n = n >> index;
+	if (n == '\0')
+		return (-1);
 	if ((n & compare) == 1)
 	{
 		return (1);

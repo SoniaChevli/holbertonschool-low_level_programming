34ab81a7264bf8ed7b04cf8238393d0edc9c4fa4
SoniaChevli <soniachevli@gmail.com>
2018-03-09 01:46:57 +0000

    Fix betty and add TRUNC

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -25,14 +25,14 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int length;
 
-	if ( filename == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
 	length = _strlen(text_content);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);

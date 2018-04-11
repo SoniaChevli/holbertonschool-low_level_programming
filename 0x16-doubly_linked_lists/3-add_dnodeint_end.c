c3797db0a21c7d595af0f957e85f59f9207b39c8
SoniaChevli <soniachevli@gmail.com>
2018-04-11 14:08:10 +0000

    Account for prev if head is null

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,10 +16,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 
+
 	if (*head == NULL)
 	{
 		*head = new;
 		new->next = NULL;
+		new->prev = NULL;
 		return (new);
 	}
 

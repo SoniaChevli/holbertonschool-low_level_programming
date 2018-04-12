f58096f1d85a9cacda3961d72f73bae96138e7cd
SoniaChevli <soniachevli@gmail.com>
2018-04-12 00:00:27 +0000

    Change order of statements, make head in loop pointer

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->prev = NULL;
 
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		new->next = NULL;
 		*head = new;
@@ -26,8 +26,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 
 	new->next = *head;
-	*head = new;
 	(*head)->prev = new;
+	*head = new;
 
 	return (new);
 }

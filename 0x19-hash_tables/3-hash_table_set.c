70c0fbc53174f0145c74102bedc65a42982719f9
Sonia Chevli <soniachevli@gmail.com>
2018-05-07 23:57:16 +0000

    Create 3-hash_table

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hashtable
+ * @ht: hash table to add or update key/value
+ * @value: value associated with the key
+ * @key: key of the string
+ *
+ *Return: 1 if succeeded. Otherwise 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = 0;
+	hash_node_t *head;
+	hash_node_t *insert;
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	head = ht->array[index];
+
+
+	insert = malloc(sizeof(hash_node_t) * (strlen(key) + 1));
+	if (insert == NULL)
+		return (0);
+
+	insert->key = strdup(key);
+	insert->value = strdup(value);
+
+	insert->next = head;
+	head = insert;
+
+	return (1);
+}

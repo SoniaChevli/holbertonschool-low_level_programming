70e9daf1457916c467c72c6ab99602dbb4d8f0f0
Sonia Chevli <soniachevli@gmail.com>
2018-05-08 02:09:16 +0000

    Create 5-hash

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print- function prints a hashtable
+ *@ht: hashtable to print
+ *
+ *Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i = 0;
+	hash_node_t *tmp;
+	unsigned int boo = 0;
+
+	printf("{");
+	while (i < ht->size)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			if (boo == 1)
+				printf(", ");
+			printf("%s: %s", tmp->key, tmp->value);
+			boo = 1;
+			tmp = tmp->next;
+		}
+		i++;
+	}
+	printf("}\n");
+}

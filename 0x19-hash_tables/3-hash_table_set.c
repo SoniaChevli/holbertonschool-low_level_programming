a93ab39b06d95cdf7f628734907b4b5926237172
Sonia Chevli <soniachevli@gmail.com>
2018-05-08 01:34:13 +0000

    Check if ht is nULL

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new;
 	hash_node_t *tmp;
 
-	if (key == NULL || value == NULL)
+	if (key == NULL || value == NULL || ht == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);

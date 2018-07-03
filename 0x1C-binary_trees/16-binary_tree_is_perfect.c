711fd7893e0cca1a931f4421b87462ddf8e5a307
Sonia Chevli <soniachevli@gmail.com>
2018-07-03 13:47:40 +0000

    first commit

---
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+#include "15-binary_tree_is_full.c"
+#include "14-binary_tree_balance.c"
+/**
+ * binary_tree_is_perfect - function that checks if binary tree is perfect
+ * @tree: pointer to the root bode of the tree to check
+ *
+ * Return: 1 if perfect. Otherwise 0.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (binary_tree_is_full(tree) == 1 && binary_tree_balance(tree) == 0)
+		return (1);
+	return (0);
+
+}
